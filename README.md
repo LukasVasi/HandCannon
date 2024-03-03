03392a979ec516329e8031636b60b05620686ecf
Lukas Vasiliauskas <lukas.vasi@gmail.com>
2024-03-03 12:22:00 +0200

    Updated player controller

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Hand Cannon
 
 Initially based on the Youtube tutorial by LegionGames: https://www.youtube.com/watch?v=A3HLeyaBCq4&list=PLQZiuyZoMHcgqP-ERsVE4x4JSFojLdcBZ&ab_channel=LegionGames 
+
+Asset attributions:
+Ground texture by upklyak on Freepik https://www.freepik.com/free-vector/game-stones-texture-pebbles-seamless-pattern_29787668.htm#fromView=search&page=1&position=2&uuid=e6982b33-894b-4d7a-8216-2cbbf36adf07
--- scenes/Player.gd
+++ scenes/Player.gd
@@ -1,9 +1,19 @@
 extends CharacterBody3D
 
-
-const SPEED = 5.0
+var speed
+const WALK_SPEED = 5.0
+const SPRINT_SPEED = 8.0
 const JUMP_VELOCITY = 4.5
 const SENSITIVITY = 0.005
+
+# Camera bob variables
+const BOB_FREQUENCY = 2.0
+const BOB_AMPLITUDE = 0.08
+var bob_t = 0.0
+
+# Fov variables
+const BASE_FOV = 75.0 # TODO: turn this into a setting
+const FOV_CHANGE = 1.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes (9.8).
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -32,6 +42,12 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
+	# Handle sprint
+	if Input.is_action_pressed("sprint"):
+		speed = SPRINT_SPEED
+	else:
+		speed = WALK_SPEED
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -40,12 +56,31 @@ func _physics_process(delta):
 	# to obtain the movement direction relative to where the player's facing
 	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
-	if direction: # if the direction is non-zero as in the player has pressed a button
-		# move with SPEED towards that direction
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else: # if the player isn't pressing anything
-		velocity.x = 0.0
-		velocity.z = 0.0
+	if is_on_floor():
+		if direction: # if the direction is non-zero as in the player has pressed a button
+			# move with SPEED towards that direction
+			velocity.x = direction.x * speed
+			velocity.z = direction.z * speed
+		else: # if the player isn't pressing anything
+			velocity.x = lerp(velocity.x, 0.0, delta * 6.0)
+			velocity.z = lerp(velocity.z, 0.0, delta * 6.0)
+	else:
+		velocity.x = lerp(velocity.x, direction.x * speed, delta * 4.0)
+		velocity.z = lerp(velocity.z, direction.z * speed, delta * 4.0)
+
+	# Head bob
+	bob_t += delta * velocity.length() * float(is_on_floor())
+	camera.transform.origin = _headbob(bob_t)
+	
+	# FOV
+	var velocity_length_clamped = clamp(velocity.length(), 0.5, SPRINT_SPEED * 2.0)
+	var target_fov = BASE_FOV + FOV_CHANGE * velocity_length_clamped
+	camera.fov = lerp(camera.fov, target_fov, delta * 8.0)
 
 	move_and_slide()
+
+func _headbob(time) -> Vector3:
+	var pos = Vector3.ZERO
+	pos.y = sin(time * BOB_FREQUENCY) * BOB_AMPLITUDE
+	pos.x = cos(time * BOB_FREQUENCY / 2) * BOB_AMPLITUDE
+	return pos
